434c7f34d3a41e48c33b55876a23660d41faf32b
Yekchey Flores <yekcheyf@gmail.com>
2026-05-19 15:28:40 +0800

    ayos navigation

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">{{ __('About us') }}</h1>
+                    <h1 class="m-0">{{ __('About us Tanginang laravel') }}</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
